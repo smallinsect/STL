ed65539e02e68b87f14e87fbf04fdd300d1009ed
smallinsect <1161851688@qq.com>
2019-02-26 21:43:55 +0800

    string的其它函数使用

--- LearSTL/MyString/MyStringMain.cpp
+++ LearSTL/MyString/MyStringMain.cpp
@@ -53,22 +53,53 @@ int main(int argv, char **argc){
 	//cin >> mstr1;
 	//cout << mstr1;
 
+	//string str("abcdef");
+	//cout << str[0] << endl;
+	//
+	//try
+	//{
+	//	cout << str.at(10) << endl;
+	//}
+	//catch(out_of_range &a)
+	//{
+	//	cout << "数组越界" << endl;
+	//}
+	//string str1 = "呵呵呵";
+	//cout << str1 << endl;
+
+	//string str("abcdefg");
+	//cout << str << endl;
+	//printf("0x%X\n", &(str.c_str()[0]));
+	//str[1] = 'a';
+	//cout << str << endl;
+	//printf("0x%X\n", &(str.c_str()[0]));
+	
+	//string str("abcdef");
+	//string str1(5, 'w');
+
+	//str.insert(2, str1, 1, 2);
+	//cout << str << endl;
+
+	//str += str1;
+	//cout << str << endl;
+
+	//str.append("嘿嘿");
+	//cout << str << endl;
+
+	////重新赋值
+	//str = "偶哈哈哈";
+	//cout << str << endl;
+
+	//str.erase(2, 2);
+	//cout << str << endl;
+
+	//其它函数
 	string str("abcdef");
-	cout << str[0] << endl;
-	
-	try
-	{
-		cout << str.at(10) << endl;
-	}
-	catch(out_of_range &a)
-	{
-		cout << "数组越界" << endl;
-	}
-	string str1 = "呵呵呵";
-	cout << str1 << endl;
-	cin >> str1;
-	
-	cout << str1 << endl;
-	printf("哈哈哈");
+	string str1("abc");
+	char arr[10] = "";
+
+	int len = str.copy(arr, 14, 2);
+	cout << arr << " " << len << endl;
+
 	return 0;
 }
